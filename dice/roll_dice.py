ab03262b17cdb12154d0a46ce1b37d9fc532963d
Krista Kernodle <kristakernodle@gmail.com>
2020-12-20 10:15:46 -0500

    $roll command assets - refactored for naming convention. Added new logic for embed

--- dice/roll_dice.py
+++ dice/roll_dice.py
@@ -1,5 +1,23 @@
 import random
+import discord
 
 
 def roll_die():
-    return random.randint(1, 6)
+    roll_assets_dir_url = "https://raw.githubusercontent.com/kristakernodle/discord-bot-simple-games/main/dice" \
+                          "/roll_assets/ "
+    die_name_convention = "_dice_magicon_nounproject.png"
+    asset_dict = {1: "one",
+                  2: "two",
+                  3: "three",
+                  4: "four",
+                  5: "five",
+                  6: "six"}
+
+    die = random.randint(1, 6)
+    die_num = asset_dict[die]
+    die_url = "".join([roll_assets_dir_url, die_num, die_name_convention])
+
+    die_img_embed = discord.Embed(author="Magicon from the Noun Project",
+                                  description=f"rolled: {die}",
+                                  url=die_url)
+    return die_img_embed
