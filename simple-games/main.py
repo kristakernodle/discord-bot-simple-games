37aa424a98976adf9dc4c10e2ee70e53dc942e60
Krista Kernodle <kristakernodle@gmail.com>
2020-12-21 19:50:31 -0500

    Rearranged application structure

---
+++ simple-games/main.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+from discord.ext import commands
+import os
+from dice import roll_dice
+
+load_dotenv()
+my_token = os.getenv('TOKEN')
+
+bot = commands.Bot(command_prefix='$')
+
+
+@bot.event
+async def on_ready():
+    print("Ready!")
+
+
+@bot.command(name="copycat",
+             help="I will repeat whatever you type after $copycat",
+             brief="Returns input (stop copying me!)")
+async def copycat(ctx, *args):
+    await ctx.channel.send(" ".join(args))
+
+
+@bot.command(name="hangman",
+             help="Begin and interact with the Hangman game",
+             brief="Play hangman!")
+async def hangman(ctx):
+    await ctx.channel.send("Hangman!")
+
+
+@bot.command(name="ping",
+             help="When you type $ping, I return pong!",
+             brief="Returns pong")
+async def ping(ctx):
+    await ctx.channel.send("pong")
+
+
+@bot.command(name="roll")
+async def roll(ctx, num_dice=1):
+    if num_dice > 10:
+        await ctx.channel.send("I can only roll a maximums of 3 dice at once!")
+    await ctx.channel.send("Rolling...")
+    await ctx.channel.send(embed=roll_dice(num_dice))
+
+
+bot.run(my_token)
